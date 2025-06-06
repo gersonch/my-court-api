e38ffdf448e0df2ad9537237b59c6f743711f8f6
Gerson Contreras <78560393+gerssoonn@users.noreply.github.com>
2025-06-06 13:03:48 -0400

    db conection

--- src/users/users.schema.ts
+++ src/users/users.schema.ts
@@ -1,8 +1,23 @@
-import { Schema } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 export const UserSchema = new Schema({
   name: String,
   email: { type: String, unique: true, required: true },
-  password: String,
+  password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
+});
+
+import type { CallbackError } from 'mongoose';
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error as CallbackError);
+  }
 });
